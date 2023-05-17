9ed5566d6ea5fec46a2c4125bbd01f0936860057
salihabrik <briksaliha@gmail.com>
2023-05-17 16:50:30 -0700

    <<simple shell>>

--- errors.c
+++ errors.c
@@ -1,5 +1,11 @@
 #include "shell.h"
-
+/**
+ * print_error - Print error message to stderr
+ * @cmd: The command name
+ * @msg: The error message
+ *
+ * Return: 1
+ */
 int	print_error(char *cmd, char *msg)
 {
 	write(2, global.name, _strlen(global.name));
