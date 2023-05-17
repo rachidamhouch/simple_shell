c7f478df76a8872967a2e23864d8fcdd38e3a08d
rachidamhouch <rachid.amhouch@gmail.com>
2023-05-17 04:21:05 +0100

    anything

--- main.c
+++ main.c
@@ -11,19 +11,13 @@ int main(int ac, char **av, char **env)
 	char	*ptr, fd = 0;
 
 	signal(SIGINT, handler);
+	global.name = av[0];
 	if (ac > 1)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
-		{
-			write(2, av[0], _strlen(av[0]));
-			write(2, ": Can't open ", _strlen(": Can't open "));
-			write(2, av[1], _strlen(av[1]));
-			write(2, "\n", 1);
-			return (1);
-		}
+			return (print_error(av[1], "Can't open it"));
 	}
-	global.name = av[0];
 	init_env(env);
 	while (1)
 	{
