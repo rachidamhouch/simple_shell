bd9c3c6819564a91b860e0d09633f43a0bc26235
salihabrik <briksaliha@gmail.com>
2023-05-20 00:47:32 +0200

    <<simple shell>>

--- cd.c
+++ cd.c
@@ -7,4 +7,21 @@
 void cd(char **cmd)
 {
 	(void)cmd;
+	if (cmd[1] == NULL)
+	{
+		const char *home_dir = getenv("HOME");
+		if (home_dir == NULL) 
+		{
+			write(STDERR_FILENO, "cd: no HOME envirronment variable set\n", 36);
+			return;
+		}
+		if (chdir(home_dir) != 0)
+		{
+			peroor("cd");
+		}
+	else if (strcpm(cmd[1], "-") == 0)
+	{
+
+	}
+	}
 }
