f23aebceb0776429166242ade62e9ca66477a544
Rachid amhouch <rachid.amhouch@gmail.com>
2023-05-20 02:49:35 +0100

    anything

--- cd.c
+++ cd.c
@@ -1,6 +1,73 @@
 #include "shell.h"
 
 /**
+ *help_cd - help cd function.
+ *@cmd: arg 1.
+ *@global: arg 2.
+ */
+void help_cd(char **cmd, global_t *global)
+{
+	char *t[4], *tmp;
+
+	t[0] = "setenv";
+	t[1] = "PWD";
+	t[3] = NULL;
+
+	if (!env_search("OLDPWD", global))
+		print_error(cmd[0], "OLDPWD not set", global);
+	else
+	{
+		tmp = getcwd(0, 0);
+		if (chdir(env_search("OLDPWD", global)))
+		{
+			perror("cd");
+			free(tmp);
+		}
+		else
+		{
+			t[2] = env_search("OLDPWD", global);
+			print(t[2], 1, 1);
+			_setenv(t, global);
+			t[1] = "OLDPWD";
+			t[2] = tmp;
+			_setenv(t, global);
+			free(tmp);
+		}
+	}
+}
+
+/**
+ *help_cd2 - help cd function.
+ *@cmd: arg 1.
+ *@global: arg 2.
+ */
+void help_cd2(char **cmd, global_t *global)
+{
+	char *t[4], *tmp;
+
+	t[0] = "setenv";
+	t[1] = "PWD";
+	t[3] = NULL;
+
+	tmp = getcwd(0, 0);
+	if (chdir(cmd[1]))
+	{
+		perror("cd");
+		free(tmp);
+	}
+	else
+	{
+		t[2] = getcwd(0, 0);
+		_setenv(t, global);
+		free(t[2]);
+		t[1] = "OLDPWD";
+		t[2] = tmp;
+		_setenv(t, global);
+		free(tmp);
+	}
+}
+
+/**
  *cd - change the current directory.
  *@cmd: arg 1.
  *@global: arg 2.
@@ -19,7 +86,7 @@ void cd(char **cmd, global_t *global)
 		else
 		{
 			tmp = getcwd(0, 0);
-			if(chdir(env_search("HOME", global)))
+			if (chdir(env_search("HOME", global)))
 			{
 				perror("cd");
 				free(tmp);
@@ -38,47 +105,8 @@ void cd(char **cmd, global_t *global)
 	else
 	{
 		if (cmd[1][0] == '-' && !cmd[1][1])
-		{
-			if (!env_search("OLDPWD", global))
-				print_error(cmd[0], "OLDPWD not set", global);
-			else
-			{
-				tmp = getcwd(0, 0);
-				if(chdir(env_search("OLDPWD", global)))
-				{
-					perror("cd");
-					free(tmp);
-				}
-				else
-				{
-					t[2] = env_search("OLDPWD", global);
-					print(t[2], 1, 1);
-					_setenv(t, global);
-					t[1] = "OLDPWD";
-					t[2] = tmp;
-					_setenv(t, global);
-					free(tmp);
-				}
-			}
-		}
+			help_cd(cmd, global);
 		else
-		{
-			tmp = getcwd(0, 0);
-			if(chdir(cmd[1]))
-			{
-				perror("cd");
-				free(tmp);
-			}
-			else
-			{
-				t[2] = getcwd(0, 0);
-				_setenv(t, global);
-				free(t[2]);
-				t[1] = "OLDPWD";
-				t[2] = tmp;
-				_setenv(t, global);
-				free(tmp);
-			}
-		}
+			help_cd2(cmd, global);
 	}
 }
