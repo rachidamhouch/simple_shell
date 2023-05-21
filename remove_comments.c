f7bb18be973c92bcf72fd5069cf48ba1e431beeb
rachidamhouch <rachid.amhouch@gmail.com>
2023-05-21 20:21:48 +0100

    anything

--- remove_comments.c
+++ remove_comments.c
@@ -21,11 +21,19 @@ char *remove_comments(char *ptr, global_t *global, int fd)
 		free_all(global);
 		exit(n);
 	}
-	while (ptr[len] && ptr[len] != '#' && ptr[len] != '\n')
-		len++;
-	str = malloc(len + 1);
-	while (ptr[i] && ptr[i] != '#' && ptr[i] != '\n')
+	while (ptr[len] && ptr[len] != '\n')
 	{
+		if (ptr[len] == '#')
+			if (ptr + len == ptr || ptr[len - 1] == ' ' || ptr[len - 1] == '\t')
+				break;
+		len++;
+	}
+	str = malloc(len + 1);
+	while (ptr[i] && ptr[i] != '\n')
+	{
+		if (ptr[i] == '#')
+			if (ptr + i == ptr || ptr[i - 1] == ' ' || ptr[i - 1] == '\t')
+				break;
 		str[i] = ptr[i];
 		i++;
 	}
