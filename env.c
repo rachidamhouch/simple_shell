48ba472b7072e4e3bf8cd51182717e9fa8317595
rachidamhouch <rachid.amhouch@gmail.com>
2023-05-24 01:21:37 +0100

    anything

--- env.c
+++ env.c
@@ -6,14 +6,18 @@
  */
 void env(char **cmd, global_t *global)
 {
-	int i;
+	env_t *ptr = global->env;
 
 	(void)cmd;
-	(void)global;
-	for (i = 0; environ[i] != NULL; i++)
+	while (ptr)
 	{
-		write(1, environ[i], strlen(environ[i]));
-		write(1, "\n", 1);
+		if (ptr->name)
+		{
+			print(ptr->name, 1, 0);
+			print("=", 1, 0);
+			print(ptr->value, 1, 1);
+		}
+		ptr = ptr->next;
 	}
 	global->exit_code = 0;
 }
