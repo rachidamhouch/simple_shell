eb2fba58b5a49edaacfb9af4d52ade63f755ebcc
rachidamhouch <rachid.amhouch@gmail.com>
2023-05-24 16:51:46 +0100

    anything

--- get_commands.c
+++ get_commands.c
@@ -14,7 +14,7 @@ char *help_get_path(char *ptr, global_t *global)
 	str2 = env_search("PWD", global);
 	if (ptr[0] == '/' || (ptr[0] == '.'))
 		return (ptr);
-	else if (_strncmp(str1, str2, _strlen(str1) + 1))
+	else if (!_strncmp(str1, str2, _strlen(str1) + 1))
 		return (ptr);
 	free(ptr);
 	return (NULL);
